3f3c248dd1aaba15991a82035961fd39daeea628
Pierre Coelho <pedro.dsc25@gmail.com>
2023-02-16 11:11:30 +0100

    added profile page for members

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
@@ -19,6 +23,37 @@ class LoginController extends AbstractController
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
             'error'         => $error,
+        ]);
+    }
+
+    #[Route('/profil', name: 'app_user_profile')]
+    public function profile(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $manager = $doctrine->getManager();
+        
+        $user = $this->getUser();
+
+        $form = $this->createFormBuilder($user)
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Mot de passe', 'hash_property_path' => 'password'],
+                'second_options' => ['label' => 'Confirmer le mot de passe'],
+                'mapped' => false,
+                'required'   => false,
+            ])
+            ->getForm();
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->flush();
+
+            $this->addFlash('success', 'Votre mot de passe a été modifié avec succès.');
+
+            return $this->redirectToRoute('app_user_profile');
+        }
+        
+        return $this->render('login/profile.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
